236b0764ee40cd74c4105db9abefa42431cc3c4a
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-05 11:43:35 -0700

    Update Solution.java

--- TriangleCount/Solution.java
+++ TriangleCount/Solution.java
@@ -3,6 +3,7 @@ public class Solution {
      * @param S: A list of integers
      * @return: An integer
      */
+     // Loop for one side, and use two pointers to search for the other two sides
     public int triangleCount(int S[]) {
         // write your code here
         if (S == null || S.length < 3) {
