00c988e5fc0715f9cf073ae98cb7e877bb672f64
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-05 11:41:00 -0700

    TwoPointersSolution

---
+++ TriangleCount/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    /**
+     * @param S: A list of integers
+     * @return: An integer
+     */
+    public int triangleCount(int S[]) {
+        // write your code here
+        if (S == null || S.length < 3) {
+            return 0;
+        }
+        Arrays.sort(S);
+        int start, end;
+        int count = 0;
+        for (int i = 0; i < S.length; i++) {
+            start = 0;
+            end = i - 1;
+            while (start < end) {
+                if (S[start] + S[end] > S[i]) {
+                    count += end - start;
+                    end--;
+                } else {
+                    start++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
