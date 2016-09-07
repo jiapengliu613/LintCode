f9be4415ae2586ac385a89b7eddafd80887f39d8
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-06 18:31:28 -0700

    Update Solution.java

--- MinSizeSubarray/Solution.java
+++ MinSizeSubarray/Solution.java
@@ -4,6 +4,12 @@ public class Solution {
      * @param s: an integer
      * @return: an integer representing the minimum size of subarray
      */
+     
+     /* 
+     Idea : If the sum from i to right is smaller than s, then the sum from i + 1 to right must be smaller than s, so this method
+     uses one loop and can avoid unnecessary repeatation, the big O is O(n)
+     the repeatation
+     */
     public int minimumSize(int[] nums, int s) {
         // write your code here
         if (nums == null || nums.length == 0) {
