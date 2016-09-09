2fbbd72ca225df736175d8f24117f6192495e96d
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-08 22:56:37 -0700

    Update Solution.java

--- MaximalRectangle/Solution.java
+++ MaximalRectangle/Solution.java
@@ -3,7 +3,7 @@ public class Solution {
      * @param matrix a boolean 2D matrix
      * @return an integer
      */
-     //Idea: http://www.cnblogs.com/lichen782/p/leetcode_maximal_rectangle.html bigO is O(m²n²), TLE in lintcode, use dp to optimize
+     //Version 1 : Idea: http://www.cnblogs.com/lichen782/p/leetcode_maximal_rectangle.html bigO is O(m²n²), TLE in lintcode, use dp to optimize
     public int maximalRectangle(boolean[][] matrix) {
         // Write your code here
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
@@ -32,7 +32,7 @@ public class Solution {
         return result;
     }
 }
-// DP  solution
+//Version 2 : DP  solution
 //Idea : preprocess the dp array, dp[i][j] records how many '1' exist before(i,j). Use dp[i][j] as width, and go upwards and downword 
 // to find the maximum height. big O is O(m*n*m). AC in leetcode but still TLE in lintcode
 public class Solution {
@@ -94,4 +94,30 @@ public class Solution {
     }
         
     
+}
+//Version 3 : dp solution, using monotone stack, imagine the matrix as a histogram and find the largest area, big O is O(m * n)
+public class Solution {
+    /**
+     * @param height: A list of integer
+     * @return: The area of largest rectangle in the histogram
+     */
+    public int largestRectangleArea(int[] height) {
+        // write your code here
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int result = 0;
+        for (int i = 0; i <= height.length; i++) {
+            int curHeight = (i == height.length) ? - 1 : height[i];
+            while (!stack.isEmpty() && height[stack.peek()] >= curHeight) {
+                int h = height[stack.peek()];
+                stack.pop();
+                int w = stack.isEmpty() ? i : i - (stack.peek() + 1);
+                result = Math.max(result, h * w);
+            }
+            stack.push(i);
+        }
+        return result;
+    }
 }
