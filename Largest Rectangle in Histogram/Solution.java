961c3d065754e4bd718871c571fd00cf28593fc0
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-08 22:23:16 -0700

    Update Solution.java

--- Largest Rectangle in Histogram/Solution.java
+++ Largest Rectangle in Histogram/Solution.java
@@ -3,6 +3,8 @@ public class Solution {
      * @param height: A list of integer
      * @return: The area of largest rectangle in the histogram
      */
+     // Idea : for each element in the array, while it is larger than the peak of the stack, keep popping the stack. so it is guaranteed 
+     //that each element in the stack is larger than the element below it. BigO is O(n) because each element is pushed and popped once.
     public int largestRectangleArea(int[] height) {
         // write your code here
         if (height == null || height.length == 0) {
