9249bb8b3149a7d90cf4161edf8c3133dde05431
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-12 19:29:00 -0700

    Update Solution.java

--- Problems/CoinsInaLine2/Solution.java
+++ Problems/CoinsInaLine2/Solution.java
@@ -29,3 +29,35 @@ public class Solution {
         return dp[0] > sum / 2;
     }
 }
+// Another dp solution, dp[i] is the largest value one can get from i to the end
+public class Solution {
+    /**
+     * @param values: an array of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int[] values) {
+        // write your code here
+        if (values == null || values.length == 0) {
+            return false;
+        }
+        if (values.length == 1) {
+            return true;
+        }
+        if (values.length == 2) {
+            return true;
+        }
+        int n = values.length;
+        int[] dp = new int[n + 1];
+        int[] sum = new int[n + 1];
+        sum[n - 1] = values[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            sum[i] = sum[i + 1] + values[i];
+        }
+        dp[n - 1] = values[n - 1];
+        dp[n - 2] = values[n - 1] + values[n - 2];
+        for (int i = n - 3; i >= 0; i--) {
+            dp[i] = sum[i] - Math.min(dp[i + 1], dp[i + 2]);
+        }
+        return dp[0] > sum[0] / 2;
+    }
+}
