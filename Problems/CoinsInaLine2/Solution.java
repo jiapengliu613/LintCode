c75bf12fa4a17a890147b3f5ca09c375524bb9e2
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-12 19:40:58 -0700

    Update Solution.java

--- Problems/CoinsInaLine2/Solution.java
+++ Problems/CoinsInaLine2/Solution.java
@@ -61,3 +61,47 @@ public class Solution {
         return dp[0] > sum[0] / 2;
     }
 }
+// memorized search
+public class Solution {
+    /**
+     * @param values: an array of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int[] values) {
+        // write your code here
+        if (values == null || values.length == 0) {
+            return false;
+        }
+        if (values.length == 1) {
+            return true;
+        }
+        if (values.length == 2) {
+            return true;
+        }
+        int n = values.length;
+        int[] dp = new int[n + 1];
+        int[] sum = new int[n + 1];
+        sum[n - 1] = values[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            sum[i] = sum[i + 1] + values[i];
+        }
+        
+        dp[0] = helper(0, values, dp, sum);
+        return dp[0] > sum[0] / 2;
+    }
+    int helper(int i, int[] values, int[] dp, int[] sum) {
+        int n = values.length;
+        if (dp[i] != 0) {
+            return dp[i];
+        }
+        if (i == n - 1) {
+            dp[n - 1] = values[n - 1];
+        } else if (i == n - 2) {
+            dp[n - 2] = values[n - 1] + values[n - 2];
+        } else {
+            dp[i] = sum[i] - Math.min(helper(i + 1, values, dp, sum), 
+                                      helper(i + 2, values, dp, sum));
+        }
+        return dp[i];
+    }
+}
