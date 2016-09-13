345a2f9c28a804c806c7b9320658adb4a03b9ec9
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-12 19:22:54 -0700

    Update Solution.java

--- Problems/CoinsInaLine2/Solution.java
+++ Problems/CoinsInaLine2/Solution.java
@@ -3,6 +3,7 @@ public class Solution {
      * @param values: an array of integers
      * @return: a boolean which equals to true if the first player will win
      */
+     //Idea : http://www.mamicode.com/info-detail-1114851.html
     public boolean firstWillWin(int[] values) {
         // write your code here
         if (values == null || values.length == 0) {
