8b64e3c448a8cef589e4cd8fab0c3f137fe34723
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-19 10:48:45 -0700

    Solution

---
+++ Problems/Minimum Adjustment Cost/Solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    /**
+     * @param A: An integer array.
+     * @param target: An integer.
+     */
+    public int MinAdjustmentCost(ArrayList<Integer> A, int target) {
+        // write your code here
+        if (A == null || A.size() == 0) {
+            return 0;
+        }
+        int n = A.size();
+        int[][] dp = new int[n][101];
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= 100; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                if (i == 0) {
+                    dp[i][j] = Math.abs(j - A.get(i));
+                } else {
+                    for (int k = 1; k <= 100; k++) {
+                        if (Math.abs(j - k) > target) {
+                            continue;
+                        }
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][k] + Math.abs(j - A.get(i)));
+                    }
+                }
+            }
+        }
+        int result = Integer.MAX_VALUE;
+        for (int i = 1; i <= 100; i++) {
+            result = Math.min(result, dp[n - 1][i]);
+        }
+        return result;
+        
+    }
+}
