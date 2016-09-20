2302f376df5f731bba2e144fb165c9650dbefa57
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-20 12:15:44 -0700

    Update Solution.java

--- Problems/Continuous Subarray Sum/Solution.java
+++ Problems/Continuous Subarray Sum/Solution.java
@@ -31,3 +31,38 @@ public class Solution {
         return result;
     }
 }
+//O(n) solution
+public class Solution {
+    /**
+     * @param A an integer array
+     * @return  A list of integers includes the index of the first number and the index of the last number
+     */
+    public ArrayList<Integer> continuousSubarraySum(int[] A) {
+        // Write your code here
+        ArrayList<Integer> result = new ArrayList<>(2);
+        if (A == null || A.length== 0) {
+            return result;
+        }
+        int n = A.length;
+        int[] dp = new int[n];
+        dp[0] = A[0];
+        int maxVal = A[0], tmpstart = 0, start = 0, end = 0;
+        for (int i = 1; i < n; i++) {
+            if (dp[i - 1] < 0) {
+                tmpstart = i;
+                dp[i] = A[i];
+            } else {
+                dp[i] = dp[i - 1] + A[i];
+            }
+            if (dp[i] > maxVal) {
+                maxVal = dp[i];
+                start = tmpstart;
+                end = i;
+            }
+            //System.out.println(start + " " + end);
+        }
+        result.add(start);
+        result.add(end);
+        return result;
+    }
+}
