1ed53b38a9cf0c83a8f084dcfdd9091562278693
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-23 18:48:10 -0700

    Solution

---
+++ Problems/Build Post Office/Solution.java
@@ -0,0 +1,50 @@
+class Number {
+    int x;
+    int y;
+    public Number(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+public class Solution {
+    /**
+     * @param grid a 2D grid
+     * @return an integer
+     */
+    public int shortestDistance(int[][] grid) {
+        // Write your code here
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int minDis = Integer.MAX_VALUE;
+        int m = grid.length;
+        int n = grid[0].length;
+        ArrayList<Number> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    list.add(new Number(i, j));
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    continue;
+                }
+                int tmp = findDis(i, j, list);
+                minDis = Math.min(minDis, tmp);
+            }
+        }
+        return minDis;
+    }
+    private int findDis(int row, int col, ArrayList<Number> list) {
+        int result = 0;
+        for (Number num : list) {
+            result += Math.abs(row - num.x);
+            result += Math.abs(col - num.y);
+        }
+        return result;
+    }
+}
