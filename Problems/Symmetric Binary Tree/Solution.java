baefbb7c8081356c90afdafc15906a340f329f38
jiapengliu613 <jiapengliu613@gmail.com>
2016-09-26 23:17:03 -0700

    Solution

---
+++ Problems/Symmetric Binary Tree/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root, the root of binary tree.
+     * @return true if it is a mirror of itself, or false.
+     */
+    public boolean isSymmetric(TreeNode root) {
+        // Write your code here
+        if (root == null) {
+            return true;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        return helper(left, right);
+    }
+    private boolean helper(TreeNode a, TreeNode b) {
+        if (a == null && b == null) {
+            return true;
+        }
+        if (a == null || b == null) {
+            return false;
+        }
+        if (a.val != b.val) {
+            return false;
+        }
+        return helper(a.left, b.right) && helper(a.right, b.left);
+    }
+}
